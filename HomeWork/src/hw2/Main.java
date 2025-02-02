340d15d5dac676a05b51428b38c0ad3a2383ac1e
Daniil Degtyarenko <danildegtyarenko90@gmail.com>
2025-02-02 20:04:07 +0200

    hw3

---
+++ HomeWork/src/hw2/Main.java
@@ -0,0 +1,38 @@
+package hw2;
+
+public class Main {
+    public static void main(String[] args) {
+        char ch = '№';
+        int number = 1;
+        String ClientName = "Alice";
+
+        System.out.println("Order " + ch + " " + number + " " + "Client: " + ClientName + ".");
+
+        String product = "smartphone";
+        System.out.println("Product: "+ product + ",");
+
+        double howMuch = 305.99d;
+        String price = "price";
+        String euro = "EUR";
+        System.out.println(price + " " + euro + " " + howMuch + ".");
+
+        String address = "Moon Street";
+        int stNum = 10;
+        System.out.println("Address: " + address + ", " + stNum + ".");
+
+        int number2 = 2;
+        String ClientName2 = "Tom";
+        System.out.println("Order " + ch + " " + number2 + " " + "Client: " + ClientName2 + ".");
+
+        String product2 = "laptop";
+        System.out.println("Product: "+ product2 + ",");
+
+        float howMuch2 = 570.95f;
+        System.out.println(price + " " + euro + " " + howMuch2 + ".");
+
+        String address2 = "Terra Street";
+        int stNum2 = 17;
+        System.out.println("Address: " + address2 + ", " + stNum2 + ".");
+
+    }
+}
